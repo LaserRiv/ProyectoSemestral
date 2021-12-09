4d35852d8c732c6048afd14bb95a0234a5de7739
LaserRiv <akasaizko@gmail.com>
2021-12-08 22:09:20 -0500

    Boton Volver de el sensor 2 junto con su programacion

--- app/src/main/java/com/example/proyectosemestral/Sensor2.java
+++ app/src/main/java/com/example/proyectosemestral/Sensor2.java
@@ -2,13 +2,32 @@ package com.example.proyectosemestral;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Sensor2 extends AppCompatActivity {
+
+    private Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor2);
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity();
+            }
+
+        });
+
+    }
+
+    private void changeActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
